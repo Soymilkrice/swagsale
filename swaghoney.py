9bb24a68bd31b48a7aa58f4adb127cfdbca25f9a
soymilkricekome <138391337+Soymilkrice@users.noreply.github.com>
2026-04-05 17:32:38 +0200

    Update swaghoney.py

--- swaghoney.py
+++ swaghoney.py
@@ -27,7 +27,7 @@ REVOLUT_TAG = "@YourRevolutTag"
 
 # Prestige Finishes
 TOS_TEXT = "✧ All sales are final. No refunds once goods are delivered.\n✧ We are not responsible for any game-side restrictions or bans.\n✧ Charging back will result in an immediate blacklist from our services."
-THUMBNAIL_URL = "https://i.imgur.com/8D76Xvj.png" 
+THUMBNAIL_URL = "https://cdn-icons-png.flaticon.com/512/561/561127.png" 
 # ------------------------------
 
 intents = discord.Intents.default()
